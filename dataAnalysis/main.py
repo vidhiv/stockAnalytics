117f955e46e6a6e8d640078b8e93cd341425539a
zbatul <zamin.batul@gmail.com>
2021-12-06 19:30:24 -0800

    updated csv column name

--- dataAnalysis/main.py
+++ dataAnalysis/main.py
@@ -45,6 +45,7 @@ def main():
         dfpred = dfreg[-len(forecast_set):]
         dfpred = dfpred.dropna(axis=1)
         #print(dfpred)
+        dfpred.columns = ["Date", "Forecasted Price"]
         dfpred[:5].to_csv("/home/ubuntu/CS623StockAnalytics/static/predictions/"+st.lower()+".csv", sep=',')
         
         # Plot forecasted prices
