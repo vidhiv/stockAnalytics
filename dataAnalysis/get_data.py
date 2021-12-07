5964cf1b2ae7bcf4d6f048872037c09cb4aa1963
zbatul <zamin.batul@gmail.com>
2021-12-07 07:32:54 -0800

    updated download function

--- dataAnalysis/get_data.py
+++ dataAnalysis/get_data.py
@@ -19,7 +19,7 @@ import math
 # 10. Airbnb - ABNB
 
 # DOWNLOAD stock data from yahoo finance api 
-def download_data(stocks):
+def download_data(stocks, p, i):
     data = yf.download(
             # tickers list or string as well
             tickers = stocks,
@@ -27,12 +27,12 @@ def download_data(stocks):
             # use "period" instead of start/end
             # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
             # (optional, default is '1mo')
-            period = "10y",
+            period = p,
 
             # fetch data by interval (including intraday if period < 60 days)
             # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
             # (optional, default is '1d')
-            interval = "1d",
+            interval = i,
 
             # group by ticker (to access via data['SPY'])
             # (optional, default is 'column')
